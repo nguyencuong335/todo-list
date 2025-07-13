4e250a2ab30dce58b982f66fd148acafe1ca2381
nguyencuong335 <cuongcvk20@gmail.com>
2025-07-13 19:21:06 +0700

    Render "tasks" ra giao diệnện

---
+++ main.js
@@ -0,0 +1,31 @@
+const tasks = [
+    // {
+    //     title: "Rửa bát",
+    //     completed: true,
+    // },
+    // {
+    //     title: "Quét nhà",
+    //     completed: false,
+    // },
+    // {
+    //     title: "Nấu cơm",
+    //     completed: false,
+    // },
+];
+
+const taskList = document.querySelector('#task-list');
+
+const html = tasks.map(
+    (task) => `
+                <li class="task-item ${task.completed ? 'completed' : ''}">
+                    <span class="task-title">${task.title}</span>
+                    <div class="task-action">
+                        <button class="task-btn edit">Edit</button>
+                        <button class="task-btn done">${task.completed ? 'Mark as undone' : 'Mark as done'}</button>
+                        <button class="task-btn delete">Delete</button>
+                    </div>
+                </li>
+`
+).join('');
+
+taskList.innerHTML = html;
