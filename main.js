ec03eb3027065854ed20b30284d4a14659e8be3d
nguyencuong335 <cuongcvk20@gmail.com>
2025-07-13 20:40:39 +0700

    Add new tasks

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
-const tasks = [
-    // {
-    //     title: "Rửa bát",
-    //     completed: true,
-    // },
-    // {
-    //     title: "Quét nhà",
-    //     completed: false,
-    // },
-    // {
-    //     title: "Nấu cơm",
-    //     completed: false,
-    // },
-];
+const tasks = [];
 
-const taskList = document.querySelector('#task-list');
+const taskList = document.querySelector("#task-list");
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
 
-const html = tasks.map(
-    (task) => `
-                <li class="task-item ${task.completed ? 'completed' : ''}">
+todoForm.onsubmit = function (e) {
+    e.preventDefault();
+    const value = todoInput.value.trim();
+
+    if (!value) {
+        return alert("Please write something");
+    }
+
+    const newTask = {
+        title: value,
+        completed: false,
+    };
+
+    tasks.push(newTask);
+
+    //re-render
+    render();
+
+    todoInput.value = "";
+};
+
+function render() {
+    const html = tasks
+        .map(
+            (task) => `
+                <li class="task-item ${task.completed ? "completed" : ""}">
                     <span class="task-title">${task.title}</span>
                     <div class="task-action">
                         <button class="task-btn edit">Edit</button>
-                        <button class="task-btn done">${task.completed ? 'Mark as undone' : 'Mark as done'}</button>
+                        <button class="task-btn done">${
+                            task.completed ? "Mark as undone" : "Mark as done"
+                        }</button>
                         <button class="task-btn delete">Delete</button>
                     </div>
                 </li>
 `
-).join('');
+        )
+        .join("");
 
-taskList.innerHTML = html;
+    taskList.innerHTML = html;
+}
+
+render();
